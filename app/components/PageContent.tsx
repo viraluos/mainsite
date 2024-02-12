22b166667c0be641223015a214be7c440193842d
Viraluos <109222224+viraluos@users.noreply.github.com>
2024-02-12 22:05:14 +0100

    asd

--- app/components/PageContent.tsx
+++ app/components/PageContent.tsx
@@ -5,7 +5,11 @@ import Header from "./Header";
 const PageContent = ({}) => {
     return(
         <div className="w-full rounded-lg bg-neutral-800">
+
             <Header className="rounded-lg"></Header>
+
+            <Footer>Footer</Footer>
+
         </div>
     );
 }
